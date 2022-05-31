e6d59d1a450f0b9aaefac646cada90a88cfa561b
Wesley Klein <36173623+wesleyklein@users.noreply.github.com>
2022-05-30 23:52:34 -0300

    Bug Removed

--- app.py
+++ app.py
@@ -11,12 +11,7 @@ def pagina_inicial():
 
 @app.route('/bug')                                                                                                                                
 def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+    return "teste do bug"
 
 if __name__ == '__main__':
     app.run()
